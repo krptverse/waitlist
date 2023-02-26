1b36436890e01b4f03277a1f56b657a3cd767dc3
fadilamanosi <fadilamanosi@gmail.com>
2023-02-26 19:05:24 +0100

    .

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const mailchimp = '@mailchimp/mailchimp_marketing';
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+
+    mailchimp.setConfig({
+        apiKey: '1773d850e9abfd81de003c2b15347599-us8',
+        server: "us8",
+    });
+
+    let message = "";
+
+    try {
+        const response = await mailchimp.lists.addListMember('d12dde555f', {
+            email_address: 'fadilamanosi@gmail.com',
+            status: "subscribed",
+            merge_fields: {
+                FNAME: 'fadil'
+            }
+        });
+        message = "subscrition was successfull"
+    } catch (error) {
+        message = error.response.body.detail;
+    }
+
     res.json({
-        hello: "hi!"
+        message: message
     });
 });
 
